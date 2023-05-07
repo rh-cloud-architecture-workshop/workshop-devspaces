a681dd467492275b6a205290b1a4f100624d3ae2
brunoNetId <bruno.meseguer@gmail.com>
2023-05-07 23:01:40 +0100

    globex-support task added

---
+++ workshop/module-camel/lab/globex-support/deploy.sh
@@ -0,0 +1,15 @@
+kamel run --name globex-support \
+routes-globed.java \
+code/factory.java \
+code/gbxlistener.java \
+-p file:code/app.properties \
+--config secret:client-amq \
+--resource file:request.jslt \
+--resource file:response.jslt \
+-d camel-quarkus-amqp \
+-d camel-quarkus-jackson \
+-d camel-quarkus-jslt \
+-d camel-quarkus-http \
+-d mvn:io.quarkiverse.messaginghub:quarkus-pooled-jms:1.1.0 \
+
+
